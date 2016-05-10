526428d11ae58f64b0ce0a73c985781188d2788a
Adrian Fijałkowski <a.m.fijalkowski@gmail.com>
2016-05-10 21:16:56 +0200

    try

--- src/main/java/com/biometryczne/signature/controllers/MainWindowController.java
+++ src/main/java/com/biometryczne/signature/controllers/MainWindowController.java
@@ -8,7 +8,13 @@ import com.biometryczne.signature.nodes.JavaFXPenNode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +25,18 @@ import java.util.ResourceBundle;
  * Created by Almi on 2016-05-09.
  */
 public class MainWindowController implements Initializable {
+
+
+    @FXML
+    private VBox jPenPane;
+    @FXML
+    private Pane xValuesChart;
+    @FXML
+    private Pane yValuesChart;
+    @FXML
+    private Pane pValuesChart;
+    @FXML
+    VBox vBoxCharts;
 
     private final static Logger log = LoggerFactory.getLogger(MainWindowController.class);
 
@@ -32,13 +50,68 @@ public class MainWindowController implements Initializable {
     private ControllerActionManager actionManager = new ControllerActionManager();
 
     public void initialize(URL location, ResourceBundle resources) {
-
+        mainSignatureCanvas = new JavaFXPenNode();
+        jPenPane.getChildren().add(mainSignatureCanvas);
     }
 
     @FXML
     public void closeWindow() {
         actionManager.setPerformer(new CloseWindowAction());
         actionManager.perform(mainWindow);
+    }
+
+    //          -- tutaj jeszcze posprzatam
+    @FXML
+    public void showCurrentSignature() {
+        LineChart<Number, Number> lineChart = new LineChart<Number, Number>(new NumberAxis(), new NumberAxis());
+
+        lineChart.setTitle("pozycja na osi X");
+        lineChart.setPrefSize(450, 200);
+
+        // X VALUES
+        XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
+        series.setNode(new Rectangle(0, 0));
+
+        for (int i = 0; i < mainSignatureCanvas.x.size(); i++) {
+            series.getData().add(new XYChart.Data(i, mainSignatureCanvas.x.get(i)));
+        }
+
+        lineChart.getData().add(series);
+//        vBoxCharts.getChildren().add(lineChart);
+
+        // Y VALUES
+        LineChart<Number, Number> lineChart2 = new LineChart<Number, Number>(new NumberAxis(), new NumberAxis());
+
+        lineChart2.setTitle("pozycja na osi X");
+        lineChart2.setPrefSize(450, 200);
+
+        series = new XYChart.Series<Number, Number>();
+        series.setNode(new Rectangle(0, 0));
+
+        for (int i = 0; i < mainSignatureCanvas.x.size(); i++) {
+            series.getData().add(new XYChart.Data(i, mainSignatureCanvas.y.get(i)));
+        }
+
+        lineChart2.getData().add(series);
+//        vBoxCharts.getChildren().add(lineChart2);
+
+        // Pressure VALUES
+        LineChart<Number, Number> lineChart3 = new LineChart<Number, Number>(new NumberAxis(), new NumberAxis());
+
+        lineChart3.setTitle("Nacisk");
+        lineChart3.setPrefSize(450, 200);
+
+        series = new XYChart.Series<Number, Number>();
+        series.setNode(new Rectangle(0, 0));
+
+        for (int i = 0; i < mainSignatureCanvas.x.size(); i++) {
+            series.getData().add(new XYChart.Data(i, mainSignatureCanvas.p.get(i)));
+        }
+
+        lineChart3.getData().add(series);
+        vBoxCharts.getChildren().clear();
+        vBoxCharts.getChildren().addAll(lineChart, lineChart2, lineChart3);
+
     }
 
     @FXML
@@ -51,7 +124,12 @@ public class MainWindowController implements Initializable {
     @FXML
     public void clearSignatureCanvas(ActionEvent actionEvent) {
         log.info("Clear Signature canvas");
-        actionManager.setPerformer(new ClearCanvasAction());
-        actionManager.perform(mainWindow);
+//        actionManager.setPerformer(new ClearCanvasAction());
+//        actionManager.perform(mainWindow);
+
+
+        System.out.println("Czyszcze!");
+        mainSignatureCanvas.clearArrays();
     }
+
 }
--- src/main/java/com/biometryczne/signature/nodes/JavaFXPenNode.java
+++ src/main/java/com/biometryczne/signature/nodes/JavaFXPenNode.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by Almi on 2016-05-09.
@@ -16,24 +18,78 @@ public class JavaFXPenNode extends SwingNode implements PenListener, ICanvasOper
 
     private final static Logger log = LoggerFactory.getLogger(JavaFXPenNode.class);
 
+    JLabel l;
+
+    private boolean allowWriting;
+
+    public ArrayList<Float> x; //pozycja x
+    public ArrayList<Float> y; //pozycja y
+    public ArrayList<Float> p; //nacisk
+
+    private int penPanelWidth = 300, penPanelHeight = 200;
+
     public JavaFXPenNode() {
         super();
 
-        setContent(createCanvas());
+        allowWriting = false;
+        l = new JLabel("");
+        l.setMinimumSize(new Dimension(penPanelWidth, penPanelHeight));
+        l.setBorder(BorderFactory.createLineBorder(new Color(200,200,200), 5));
+        l.setVerticalTextPosition(SwingConstants.CENTER);
+
+        x = new ArrayList<Float>();
+        y = new ArrayList<Float>();
+        p = new ArrayList<Float>();
+
+        setContent(l);
+//        setContent(createCanvas());
         AwtPenToolkit.addPenListener(getContent(), this);
     }
 
     public void penButtonEvent(PButtonEvent ev) {
-        System.out.println(ev);
-    }
+//        System.out.println(ev);
+
+        allowWriting = true;
+        System.out.println("\tMozna Pisać");
+   }
 
     public void penKindEvent(PKindEvent ev) {
-        System.out.println(ev);
+//        System.out.println(ev);
     }
 
     public void penLevelEvent(PLevelEvent ev) {
-        System.out.println(ev);
-        System.out.println("Pressure = " + ev.pen.getLevelValue(PLevel.Type.PRESSURE));
+//        System.out.println(ev);
+//        System.out.println("Pressure = " + ev.pen.getLevelValue(PLevel.Type.PRESSURE));
+
+        String tmpStr = "";
+//        System.out.println(ev);
+//        if (ev.pen.getButtonValue(PButton.Type.LEFT))
+        if (allowWriting) {
+            x.add(ev.pen.getLevelValue(PLevel.Type.X));
+            y.add(ev.pen.getLevelValue(PLevel.Type.Y));
+            p.add(ev.pen.getLevelValue(PLevel.Type.PRESSURE));
+
+
+            tmpStr = (""
+                    + "X = " + ev.pen.getLevelValue(PLevel.Type.X)
+                    + "Y = " + ev.pen.getLevelValue(PLevel.Type.Y)
+                    + " Pressure = " + ev.pen.getLevelValue(PLevel.Type.PRESSURE)
+//                    + " LeftButton = " + ev.pen.getButtonValue(PButton.Type.LEFT)
+//                    + " RightButton = " + ev.pen.getButtonValue(PButton.Type.RIGHT)
+                    + "");
+
+
+            System.out.println(tmpStr);
+            l.setText(tmpStr);
+        }
+        //poza obszarem panelu wylacz mozliwosc pisania (nie pobiera danych)
+        if (ev.pen.getLevelValue(PLevel.Type.X) < 5 | ev.pen.getLevelValue(PLevel.Type.X) > penPanelWidth-5
+                | ev.pen.getLevelValue(PLevel.Type.Y) < 5 | ev.pen.getLevelValue(PLevel.Type.Y) > penPanelHeight-5) {
+            allowWriting = false;
+            System.out.println("\tNIE Mozna Pisać");
+        }
+
+
     }
 
     public void penScrollEvent(PScrollEvent ev) {
@@ -41,7 +97,7 @@ public class JavaFXPenNode extends SwingNode implements PenListener, ICanvasOper
     }
 
     public void penTock(long availableMillis) {
-        System.out.println("TOCK - available period fraction: "+availableMillis);
+//        System.out.println("TOCK - available period fraction: "+availableMillis);
     }
 
     private JComponent createCanvas() {
@@ -63,5 +119,11 @@ public class JavaFXPenNode extends SwingNode implements PenListener, ICanvasOper
         setContent(createCanvas());
         AwtPenToolkit.addPenListener(getContent(), this);
 
+    }
+
+    public void clearArrays() {
+        x.clear();
+        y.clear();
+        p.clear();
     }
 }
